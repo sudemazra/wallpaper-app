8754329238af1fb518b12f9147252f1f86f9dfe2
sudemazra <emirdagsudemazra@gmail.com>
2025-03-10 14:33:46 +0300

    add design

---
+++ app/src/main/java/com/example/wallpaperapp/MainActivity.java
@@ -0,0 +1,30 @@
+
+package com.example.wallpaperapp;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.white)); // beyaz
+        }
+
+    }
+}
